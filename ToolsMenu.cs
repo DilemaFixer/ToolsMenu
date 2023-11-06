1fa2b3da9fd8d939d5edff4150996ca303a8a3d6
DilemaFixer <109586143+DilemaFixer@users.noreply.github.com>
2023-11-06 13:28:43 +0200

    fix script

--- ToolsMenu.cs
+++ ToolsMenu.cs
@@ -10,15 +10,18 @@ namespace Dilemma
         public static void CreateToolsMenu()
         {
             Directory.CreateDirectory(Path.Combine(Application.dataPath, "_Project"));
+            AssetDatabase.Refresh();
             Dir("_Project", "[0]Source", "[1]Resources", "[2]Unity Assets", "[3]Temporary");
+            AssetDatabase.Refresh();
             Dir("_Project/[1]Resources", "Prefabs", "Scenes", "Models", "Textures");
+            AssetDatabase.Refresh();
         }
 
         public static void Dir(string root, params string[] paths)
         {
             string fullPath = Path.Combine(Application.dataPath, root);
             foreach (var path in paths)
-                Directory.CreateDirectory(Path.Combine(root, path));
+                Directory.CreateDirectory(Path.Combine(fullPath, path));
         }
 
     }
