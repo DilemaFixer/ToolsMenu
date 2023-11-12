d3ea8533ac45143f66c39d429c98c1bf15885a02
DilemaFixer <109586143+DilemaFixer@users.noreply.github.com>
2023-11-12 18:09:50 +0200

    regreit

--- ToolsMenu.cs
+++ ToolsMenu.cs
@@ -11,13 +11,11 @@ namespace Dilemma
         {
             Directory.CreateDirectory(Path.Combine(Application.dataPath, "_Project"));
             AssetDatabase.Refresh();
-            Dir("_Project", "[0]Source", "[1]Resources", "[2]Unity Assets", "[3]Temporary");
+            Dir("_Project", "Source", "[1]Resources", "[2]Unity Assets", "[3]Temporary");
             AssetDatabase.Refresh();
-            Dir("_Project/[1]Resources", "Prefabs", "Scenes", "Models", "Textures");
+            Dir(@"_Project\[1]Resources", "Prefabs", "Scenes", "Models", "Textures");
             AssetDatabase.Refresh();
-            Dir("_Project/[0]Source", "Editor" , "Game");
-            CreateAssemblyDefinition(Path.Combine(Application.dataPath, "_Project/[0]Source/Editor") , "Editor")
-            CreateAssemblyDefinition(Path.Combine(Application.dataPath, "_Project/[0]Source/Game") , "Game")
+            Dir(@"_Project\[0]Source", "Editor" , "Game");
             AssetDatabase.Refresh();
         }
 
@@ -26,28 +24,6 @@ namespace Dilemma
             string fullPath = Path.Combine(Application.dataPath, root);
             foreach (var path in paths)
                 Directory.CreateDirectory(Path.Combine(fullPath, path));
-        }
-
-        private static void CreateAssemblyDefinition(string path , string nameOfAssemblyDefinition)
-        {
-            System.IO.File.WriteAllText(path, GetDefaultAssemblyDefinitionContent(nameOfAssemblyDefinition));
-        }
-
-        private string GetDefaultAssemblyDefinitionContent(string name)
-        {
-            return @"{
-            ""name"": ""Assembly-CSharp"",
-            ""references"": [],
-            ""includePlatforms"": [],
-            ""excludePlatforms"": [],
-            ""allowUnsafeCode"": false,
-            ""overrideReferences"": false,
-            ""precompiledReferences"": [],
-            ""autoReferenced"": true,
-            ""defineConstraints"": [],
-            ""versionDefines"": [],
-            ""noEngineReferences"": false
-        }";
         }
     }
 }
