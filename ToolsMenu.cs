ecf6a935565e36f5df5ea189b251c54afab68ee8
DilemaFixer <109586143+DilemaFixer@users.noreply.github.com>
2023-11-12 17:02:19 +0200

    Add Assembly Definition

--- ToolsMenu.cs
+++ ToolsMenu.cs
@@ -15,15 +15,40 @@ namespace Dilemma
             AssetDatabase.Refresh();
             Dir("_Project/[1]Resources", "Prefabs", "Scenes", "Models", "Textures");
             AssetDatabase.Refresh();
+            Dir("_Project/[0]Source", "Editor" , "Game");
+            CreateAssemblyDefinition(Path.Combine(Application.dataPath, "_Project/[0]Source/Editor") , "Editor")
+            CreateAssemblyDefinition(Path.Combine(Application.dataPath, "_Project/[0]Source/Game") , "Game")
+            AssetDatabase.Refresh();
         }
 
-        public static void Dir(string root, params string[] paths)
+        private static void Dir(string root, params string[] paths)
         {
             string fullPath = Path.Combine(Application.dataPath, root);
             foreach (var path in paths)
                 Directory.CreateDirectory(Path.Combine(fullPath, path));
         }
 
+        private static void CreateAssemblyDefinition(string path , string nameOfAssemblyDefinition)
+        {
+            System.IO.File.WriteAllText(path, GetDefaultAssemblyDefinitionContent(nameOfAssemblyDefinition));
+        }
+
+        private string GetDefaultAssemblyDefinitionContent(string name)
+        {
+            return @"{
+            ""name"": ""Assembly-CSharp"",
+            ""references"": [],
+            ""includePlatforms"": [],
+            ""excludePlatforms"": [],
+            ""allowUnsafeCode"": false,
+            ""overrideReferences"": false,
+            ""precompiledReferences"": [],
+            ""autoReferenced"": true,
+            ""defineConstraints"": [],
+            ""versionDefines"": [],
+            ""noEngineReferences"": false
+        }";
+        }
     }
 }
 
